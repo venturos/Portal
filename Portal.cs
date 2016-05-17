7e490aa8a101cacab109a841eac6aeb8d6356a15
Lutsyk <makc199710@me.com>
2016-05-17 15:18:00 +0300

    FOR IVANOV

---
+++ Portal.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    TeleportCamera firstCamera, secondCamera;
+
+    [SerializeField]
+    MeshRenderer firstRenderer, secondRenderer;
+
+    [SerializeField]
+    Transform firstPosition, secondPosition;
+
+    [SerializeField]
+    Portal connectedPortal;
+
+    void Start()
+    {
+        SetupPortal();
+    }
+
+    void SetupPortal()
+    {
+        if (connectedPortal == null)
+            return;
+
+        firstCamera.SetMeshRenderer(connectedPortal.firstRenderer);
+        secondCamera.SetMeshRenderer(connectedPortal.secondRenderer);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (connectedPortal == null)
+            return;
+
+        Character character = other.GetComponent<Character>();
+        if (character == null)
+            return;
+
+        bool isFirst = Vector3.Dot(transform.forward, transform.position - other.transform.position) < 0;
+
+        if (isFirst)
+            character.Teleport(connectedPortal.firstPosition.position);
+        else
+            character.Teleport(connectedPortal.secondPosition.position);
+
+        var cameras = FindObjectsOfType<MainCamera>();
+
+        foreach (var camera in cameras)
+            camera.Blur();
+    }
+}
---
+++ TeleportCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportCamera : MonoBehaviour
+{
+    Camera camera;
+    
+    RenderTexture texture;
+
+    void Awake ()
+    {
+        camera = GetComponent<Camera>();
+
+        texture = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+
+        camera.aspect = 1;
+
+        camera.targetTexture = texture;
+    }
+
+    public void SetMeshRenderer(MeshRenderer renderer)
+    {
+        renderer.material.mainTexture = texture;
+    }
+
+}
